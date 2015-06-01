9f5f0271c382d93854a66b63f29a20d8a4280a20
Jan Weitz <jan@iosphere.de>
2015-06-01 22:10:09 +0200

    Updates to openssl 1.0.2a using commit: c6b17fa from https://github.com/x2on/OpenSSL-for-iPhone

--- openssl/build-libssl.sh
+++ openssl/build-libssl.sh
@@ -21,7 +21,7 @@
 ###########################################################################
 #  Change values here													  #
 #				
-VERSION="1.0.1j"													      #
+VERSION="1.0.2a"													      #
 SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`														  #
 #																		  #
 ###########################################################################
@@ -61,7 +61,7 @@ esac
 set -e
 if [ ! -e openssl-${VERSION}.tar.gz ]; then
 	echo "Downloading openssl-${VERSION}.tar.gz"
-    curl -O http://www.openssl.org/source/openssl-${VERSION}.tar.gz
+    curl -O https://www.openssl.org/source/openssl-${VERSION}.tar.gz
 else
 	echo "Using openssl-${VERSION}.tar.gz"
 fi
